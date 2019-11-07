82ed55f1428237a162544641a866572ad500554d
Sanket Chaudhari <SanketC2@citiustech.com>
2019-11-07 19:24:10 +0530

    Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
-import { Component, OnInit, OnChanges, Inject } from '@angular/core';
-import{FormsModule, ReactiveFormsModule, FormGroup, FormControl} from '@angular/forms';
-import Tribute from 'tributejs';
-import { DOCUMENT } from '@angular/common';
+import { Component, OnInit} from '@angular/core';
+
 
 
 
@@ -12,14 +10,19 @@ import { DOCUMENT } from '@angular/common';
 })
 export class AppComponent implements OnInit {
   ngOnInit(): void {
+    // tslint:disable-next-line: no-unused-expression
     new Mentionify(
       document.getElementById('textarea'),
       document.getElementById('menu'),
+      // tslint:disable-next-line: no-use-before-declare
       resolveFn,
+      // tslint:disable-next-line: no-use-before-declare
       replaceFn,
+      // tslint:disable-next-line: no-use-before-declare
       menuItemFn
     );
   }
+  // tslint:disable-next-line: member-ordering
   properties = [
     'direction',
     'boxSizing',
@@ -60,6 +63,7 @@ export class AppComponent implements OnInit {
     'MozTabSize',
   ];
 
+   // tslint:disable-next-line: member-ordering
    isFirefox = typeof window !== 'undefined' && window['mozInnerScreenX'] != null;
 
   /**
@@ -67,6 +71,7 @@ export class AppComponent implements OnInit {
    * @param {number} position
    */
   getCaretCoordinates(element, position) {
+    // // console.log('posiiton' + position);
     const div = document.createElement('div');
     document.body.appendChild(div);
 
@@ -83,8 +88,10 @@ export class AppComponent implements OnInit {
     });
 
     if (this.isFirefox) {
-      if (element.scrollHeight > parseInt(computed.height))
+      // tslint:disable-next-line: radix
+      if (element.scrollHeight > parseInt(computed.height)) {
         style.overflowY = 'scroll';
+      }
     } else {
       style.overflow = 'hidden';
     }
@@ -94,15 +101,20 @@ export class AppComponent implements OnInit {
     const span = document.createElement('span');
     span.textContent = element.value.substring(position) || '.';
     div.appendChild(span);
+    // // console.log('span ' + span.offsetHeight);
 
     const coordinates = {
+      // tslint:disable-next-line: radix
       top: span.offsetTop + parseInt(computed['borderTopWidth']),
+      // tslint:disable-next-line: radix
       left: span.offsetLeft + parseInt(computed['borderLeftWidth']),
       // height: parseInt(computed['lineHeight'])
       height: span.offsetHeight
     };
 
-    div.remove();
+    // // console.log('coord top ' + coordinates.top + 'coord left ' + coordinates.left);
+
+    // div.remove();
 
     return coordinates;
   }
@@ -113,6 +125,7 @@ export class AppComponent implements OnInit {
 
 
 class Mentionify {
+  // tslint:disable-next-line: no-shadowed-variable
   constructor(ref, menuRef, resolveFn, replaceFn, menuItemFn) {
     this.ref = ref;
     this.menuRef = menuRef;
@@ -127,9 +140,12 @@ class Mentionify {
     this.onInput = this.onInput.bind(this);
     this.onKeyDown = this.onKeyDown.bind(this);
     this.renderMenu = this.renderMenu.bind(this);
+    this.onClick = this.onClick.bind(this);
 
     this.ref.addEventListener('input', this.onInput);
     this.ref.addEventListener('keydown', this.onKeyDown);
+    this.ref.addEventListener('click', this.onClick);
+    this.closeMenu();
   }
   ref: any;
   menuRef: any;
@@ -142,6 +158,7 @@ class Mentionify {
   triggerIdx: any;
   active: number;
   query: any;
+  clicked = false;
 
   async makeOptions(query) {
     const options = await this.resolveFn(query);
@@ -163,6 +180,11 @@ class Mentionify {
     }, 0);
   }
 
+  onClick() {
+    this.clicked = true;
+    this.closeMenu();
+  }
+
   selectItem(active) {
     return () => {
       const preMention = this.ref.value.substr(0, this.triggerIdx);
@@ -178,8 +200,9 @@ class Mentionify {
       this.ref.focus();
     };
   }
-  onInput(ev: any) {
-    const positionIndex = this.ref.selectionStart;
+  onInput(ev) {
+    let positionIndex = this.ref.selectionStart;
+    // console.log(positionIndex + 'ref selection start');
     const textBeforeCaret = this.ref.value.slice(0, positionIndex);
     const tokens = textBeforeCaret.split(/\s/);
     const preLastToken = tokens[tokens.length - 2];
@@ -187,42 +210,50 @@ class Mentionify {
     const triggerIdx = textBeforeCaret.endsWith(lastToken)
       ? textBeforeCaret.length - lastToken.length
       : -1;
-    console.log(triggerIdx);
+    // console.log(triggerIdx);
     let triggerIdx2: number;
     let maybeTrigger2;
     let keystrokeTriggered;
     const maybeTrigger = textBeforeCaret[triggerIdx];
-    if (preLastToken) {
+    if (preLastToken && !this.clicked) {
       triggerIdx2 = textBeforeCaret.length - lastToken.length - preLastToken.length - 1;
       maybeTrigger2 = textBeforeCaret[triggerIdx2];
       keystrokeTriggered = maybeTrigger === '@' || maybeTrigger2 === '@';
-      console.log('keystroke' + maybeTrigger2);
+      // console.log('keystroke' + maybeTrigger2);
     } else {
       keystrokeTriggered = maybeTrigger === '@';
     }
     // const keystrokeTriggered = maybeTrigger === '@';
 
     if (!keystrokeTriggered) {
-      console.log('close');
+      // console.log('close');
       this.closeMenu();
       return;
     }
-    console.log(triggerIdx + 1);
-    if (maybeTrigger2 === '@') {
+    // console.log(triggerIdx + 1);
+    // console.log('may be trigger 2 ' + maybeTrigger2);
+    if (maybeTrigger2 === '@' && maybeTrigger === '@') {
+      this.triggerIdx = triggerIdx;
+      this.query = textBeforeCaret.slice(triggerIdx + 1);
+    } else if (maybeTrigger2 === '@') {
       this.triggerIdx = triggerIdx2;
       this.query = textBeforeCaret.slice(triggerIdx2 + 1);
+      // console.log('query index ' + this.query.substring(this.query.indexOf('@') + 1));
+      // console.log('query ' + this.query + 'triggerindx 2 ' + triggerIdx2 + 'triggeridx1 ' + triggerIdx);
+      // this.query = this.query.substring(this.query.indexOf('@') + 1);
     } else {
       this.triggerIdx = triggerIdx;
       this.query = textBeforeCaret.slice(triggerIdx + 1);
+      this.clicked = false;
     }
     this.makeOptions(this.query);
 
-    let mc = new AppComponent();
+    const mc = new AppComponent();
+    positionIndex = this.triggerIdx;
     const coords = mc.getCaretCoordinates(this.ref, positionIndex);
     const { top, left } = this.ref.getBoundingClientRect();
-
-    console.log('ht' + coords.height);
-
+    // console.log('top ' + (coords.top + top - this.ref.scrollTop) + 'left ' + (coords.left + left + this.ref.scrollLeft));
+    // console.log('coords height  ' + coords.height);
     setTimeout(() => {
       this.active = 0;
       this.left = window.scrollX  + coords.left + left + this.ref.scrollLeft;
@@ -264,7 +295,7 @@ class Mentionify {
       this.menuRef.hidden = true;
       return;
     }
-
+    // console.log('menuref left ' + this.left + 'menuref top ' + this.top);
     this.menuRef.style.left = this.left + 'px';
     this.menuRef.style.top = this.top + 'px';
     this.menuRef.innerHTML = '';
@@ -289,17 +320,21 @@ const users = [
   { username: 'jane do' }
 ];
 
-const resolveFn = prefix => prefix === '' ? users : users.filter(user => user.username.includes(prefix));
+const resolveFn = prefix => prefix === ''
+  ? users
+  : users.filter(user => user.username.includes(prefix));
 
-const replaceFn = (user, trigger) => `${trigger}${user.username}`;
+const replaceFn = (user, trigger) =>`${trigger}${user.username}`;
 
 const menuItemFn = (user, setItem, selected) => {
   const div = document.createElement('div');
   div.setAttribute('role', 'option');
   div.className = 'menu';
   if (selected) {
-    div.classList.add('alert-secondary');
+    // div.classList.add('alert-secondary');
     div.setAttribute('aria-selected', '');
+    div.classList.add('selected');
+    div.style.backgroundColor = 'rgba(220, 220, 221, 0.692)';
 
   }
   div.textContent =  user.username;
